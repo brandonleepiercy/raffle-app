367ecc4828c2db77041600b08c52a76d9c4aac94
Brandon Piercy <b.piercy567@gmail.com>
2020-12-26 17:04:28 -0800

    server now spins and the submission function works and verifies properly

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+require("./app/routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT "+ PORT);
+});

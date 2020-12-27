90e693751ecd2322838111418835c7bb4efdc037
Brandon Piercy <b.piercy567@gmail.com>
2020-12-27 10:18:36 -0800

    created html routing so theres a page for the administrator to access the backend and view all entries

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.static("app/public"));
 
 require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT "+ PORT);
